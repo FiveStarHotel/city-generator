c20995056e098967ed9cca454e5e0e862bfd9f97
FiveStarHotel <nekitgogosh@gmail.com>
2025-12-03 21:41:25 +0700

    Add __str__, get_tuple methods. Commented class' fields

--- Point.py
+++ Point.py
@@ -1,5 +1,21 @@
 class Point:
 
     def __init__(self, i:int, j:int):
-        self.i = i
-        self.j = j
+        self.i = i # Строка (Y)
+        self.j = j # Столбец (X)
+
+    def __str__(self):
+        """
+        Возвращает строковое представление точки в формате:
+        (x,y)
+        """
+
+        return f"({self.j},{self.i})"
+
+    def get_tuple(self) -> tuple:
+        """
+        Возвращает точку в виде кортежа, который имеет следующую структуру:
+        (x,y)
+        """
+
+        return self.j, self.i
